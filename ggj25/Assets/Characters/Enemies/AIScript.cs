1f1d67993473ae0988056a4d286c0d66a55f5042
Lorenzo <disalvo.lorenzo@protonmail.com>
2025-01-25 21:54:53 +0100

    Add first prototype of Enemy AI

---
+++ ggj25/Assets/Characters/Enemies/AIScript.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Threading;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Characters.Enemies
+{
+    public class AIScript : MonoBehaviour
+    {
+        public enum EnemyType
+        {
+            Small,
+            Medium,
+            Large,
+            Muskrat
+        }
+
+        [SerializeField] public EnemyType type;
+        private GameObject _player;
+
+        public float step;
+        public bool playerSpotted;
+
+        void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player");
+            switch (type)
+            {
+                case EnemyType.Small:
+                    step = 7;
+                    break;
+                case EnemyType.Large:
+                    step = 3;
+                    break;
+                case EnemyType.Muskrat:
+                    step = 6;
+                    break;
+                case EnemyType.Medium:
+                default:
+                    break;
+            }
+        }
+
+        void Update()
+        {
+            if (!playerSpotted) CheckForPlayer();
+            CheckBehaviour();
+        }
+
+        private void CheckForPlayer()
+        {
+            var pos = transform.position;
+            var playerPos = _player.transform.position;
+            playerSpotted = Vector3.Distance(pos, playerPos) < 10;
+        }
+
+        private void CheckBehaviour()
+        {
+            if (!playerSpotted) RandomMove();
+            switch (type)
+            {
+                case EnemyType.Small:
+                    DoSmall();
+                    break;
+                case EnemyType.Medium:
+                    DoMedium();
+                    break;
+                case EnemyType.Large:
+                    DoLarge();
+                    break;
+                case EnemyType.Muskrat:
+                    DoMuskrat();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            Thread.Sleep((int)(Random.value * 2000));
+        }
+
+        private void RandomMove()
+        {
+            var ver = -1 ^ Random.Range(1, 3);
+            var hor = -1 ^ Random.Range(1, 3);
+            var dir = new Vector3(1 * ver, 1 * hor, transform.position.z);
+            transform.position += dir * (step * Time.deltaTime);
+        }
+
+        private void DoMedium()
+        {
+            if (!playerSpotted) return; 
+            
+            transform.position += _player.transform.position * (step * Time.deltaTime);
+            Attack();
+        }
+
+        private void DoLarge()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DoSmall()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DoMuskrat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCollisionEnter(Collision other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
